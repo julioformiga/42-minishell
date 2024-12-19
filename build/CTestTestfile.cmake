168583191eb0ac6e5a6284842b80f29b2a169591
Julio Formiga <julio.formiga@gmail.com>
2024-12-19 21:20:31 +0100

    chg: rm build/

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/julio/dev/42/cursus/minishell
-# Build directory: /home/julio/dev/42/cursus/minishell/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(TestMinishell "/home/julio/dev/42/cursus/minishell/build/test_minishell")
-set_tests_properties(TestMinishell PROPERTIES  _BACKTRACE_TRIPLES "/home/julio/dev/42/cursus/minishell/CMakeLists.txt;58;add_test;/home/julio/dev/42/cursus/minishell/CMakeLists.txt;0;")
-subdirs("_deps/googletest-build")
--- build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/julio/dev/42/cursus/minishell/build/_deps/googletest-src/googletest
-# Build directory: /home/julio/dev/42/cursus/minishell/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
