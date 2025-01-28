5e7a7fc229503f5b63db55962c54bcc7b0a6be9f
Julio Formiga <julio.formiga@gmail.com>
2025-01-28 02:12:54 +0100

    del: printf

--- src/builtin/utils.c
+++ src/builtin/utils.c
@@ -61,6 +61,5 @@ int	execute_builtin(t_cmd *cmd, t_env *env, int input_fd, int output_fd)
 	dup2(stdin_backup, STDIN_FILENO);
 	close(stdout_backup);
 	close(stdin_backup);
-	printf("result builtin: %d\n", result);
 	return (g_signal = result, result);
 }
