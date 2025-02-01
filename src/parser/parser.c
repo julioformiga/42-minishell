d0b9c0220ee6ccd9cbf4151d4fc4558ca1bcda08
Sabino Carlucci <scarlucc@c2r2p2.42firenze.it>
2025-02-01 11:28:04 +0100

    fixed leak with consecutive operators (echo | |), starting change parsing to interpret || as | |

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: scarlucc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 17:23:25 by scarlucc          #+#    #+#             */
-/*   Updated: 2025/01/31 20:28:18 by scarlucc         ###   ########.fr       */
+/*   Updated: 2025/02/01 11:24:52 by scarlucc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,11 @@ char	*handle_token(char **rl, t_env *env, int *val, int *i)
 {
 	if (**rl == '\'' || **rl == '"')
 		return (extract_quoted_token(rl, env));
-	if (is_operator_start(**rl))
+	if (is_operator_start(**rl))//forse mettere get_operator_type
 	{
 		//if ()
 			val[*i] = 1;
-		return (extract_operator(rl));
+		return (extract_operator(rl));//in extract_operator togli casistica ||
 	}
 	return (extract_word(rl, env));
 }
@@ -41,7 +41,7 @@ int	handle_operator(char *token, char **tokens, int *i)
 	char	*tmp;
 
 	tmp = ft_strjoin(tokens[*i], token);
-	free(tokens[*i]);
+	free(tokens[*i]);//forse ridondante
 	tokens[*i] = ft_strdup(tmp);
 	if (check_previuos_op(token, tokens, *i))
 		return (free(tmp), 1);
@@ -59,7 +59,7 @@ void	update_tokens(char **tokens, int *i, char *token, char *rl)
 	tokens[*i] = ft_strdup(tmp);
 	if (*rl && (ft_isspace(*rl) || is_operator_start(*rl)))//e se inizia con spazio?
 		tokens[++(*i)] = ft_strdup("");
-	free(tmp);//liberare tmp?
+	free(tmp);
 	free(token);
 }
 
