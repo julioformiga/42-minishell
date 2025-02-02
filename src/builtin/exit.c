c6c4eb4b2e7373165d08b158b24dd745bdd04855
Sabino Carlucci <scarlucc@c2r4p8.42firenze.it>
2025-02-02 22:12:46 +0100

    fixed order of ifs and exit code % 256

--- src/builtin/exit.c
+++ src/builtin/exit.c
@@ -3,30 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: julio.formiga <julio.formiga@gmail.com>    +#+  +:+       +#+        */
+/*   By: scarlucc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/21 00:14:41 by julio.formiga     #+#    #+#             */
-/*   Updated: 2024/09/21 00:14:41 by julio.formiga    ###   ########.fr       */
+/*   Created: 2024/09/21 00:14:41 by julio.formi       #+#    #+#             */
+/*   Updated: 2025/02/02 22:08:44 by scarlucc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	parse_exit_code(char **args)
+static int	parse_exit_code(char **args, int i)
 {
 	if (!args)
 		return (0);
-	if (ft_array_len(args) > 1)
-	{
-		printf("minishell\nexit: too many arguments\n");
-		return (1);
-	}
 	if (args[0])
 	{
-		if (ft_isdigit(args[0][0]) || args[0][0] == '-')
-			return (ft_atoi(args[0]));
-		printf("minishell: exit: %s: numeric argument required\n", args[0]);
-		return (2);
+		while (ft_isdigit(args[0][i]))
+			i++;
+		if (args[0][i] == '\0')
+		{
+			if (ft_array_len(args) > 1)
+			{
+				printf("minishell: exit: too many arguments\n");
+				return (1);
+			}
+		}
+		else
+		{
+			printf("minishell: exit: %s: numeric argument required\n", args[0]);
+			return (2);
+		}
+		return (ft_atoi(args[0]));
 	}
 	return (0);
 }
@@ -34,12 +41,18 @@ static int	parse_exit_code(char **args)
 int	builtin_exit(t_cmd *cmd, t_env *env)
 {
 	int	exit_code;
+	int	i;
 
-	exit_code = parse_exit_code(cmd->cmd->args);
+	if (cmd->cmd->args && cmd->cmd->args[0][0] == '-')
+		i = 1;
+	else
+		i = 0;
+	printf("exit\n");
+	exit_code = parse_exit_code(cmd->cmd->args, i);
 	if (exit_code == 1)
 		return (exit_code);
 	rl_clear_history();
 	free_cmd(cmd);
 	env_free(env);
-	exit(exit_code);
+	exit(exit_code % 256);
 }
