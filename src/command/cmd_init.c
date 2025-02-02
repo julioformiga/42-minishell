6be056e076096c6d08758f4aa1db3f6f76b38ff5
Julio Formiga <julio.formiga@gmail.com>
2025-02-02 21:49:47 +0100

    fix(:')): ., .., ./ & / (desperation mode activated)

--- src/command/cmd_init.c
+++ src/command/cmd_init.c
@@ -70,6 +70,17 @@ int	cmd_exec_update_position_cmd(t_cmd **cmdtmp)
 
 int	cmd_setup(t_cmd *cmd, t_env *env, char ***args, char **full_path)
 {
+	if (!cmd || !env)
+		return (1);
+	if (ft_strcmp(cmd->cmd->exec, ".") == 0
+		|| ft_strcmp(cmd->cmd->exec, "..") == 0
+		|| ft_strcmp(cmd->cmd->exec, "./") == 0
+		|| ft_strcmp(cmd->cmd->exec, "/") == 0)
+	{
+		free_cmd(cmd);
+		env_free(env);
+		return (1);
+	}
 	*full_path = cmd_check(cmd, env);
 	if (!*full_path)
 		return (g_signal);
